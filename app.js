e0a51d884617685c09cffb950462194ef1992cb1
Rutini <vl.patsuta@gmail.com>
2020-07-22 23:04:28 +0300

    add some new answers and modify get valuta method

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const {telegram_bot_token, rumunyChatId} = require('./config/telegramConfig');
+const { telegram_bot_token, rumunyChatId } = require('./config/telegramConfig');
+const { defaultStickers,
+        vinnik_ogo,
+        stonks_up,
+        stonks_down } = require('./constants/stickers');
+const { on_sticker_answers, on_voice_answers, on_video_answers } = require('./constants/answers');
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(telegram_bot_token, {polling: true, onlyFirstMatch: true});
 
 const axios = require('axios');
+const { Random } = require('random-js');
+const random = new Random();
 
 const startCommand = '/start';
 const getValutaCommandPrivate = '/valuta';
@@ -17,15 +24,30 @@ const getogoSticker = 'ого';
 
 const cron = require('node-cron');
 
+let previsousData;
+
+(async function () {
+    previsousData = await axios.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+})();
+
 async function sendCurrentValues(chatId) {
-    const {data: [{buy: dollarBuy, sale: dollarSale}, {buy: euroBuy, sale: euroSale}]} = await axios.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+    const {data: [{buy: newDollarBuy, sale: newDollarSale}, {buy: newEuroBuy, sale: newEuroSale}]} = await axios.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+    const {data: [{buy: oldDollarBuy, sale: oldDollarSale}, {buy: oldEuroBuy, sale: oldEuroSale}]} = previsousData;
+
+    const buyDollarDiff = newDollarBuy - oldDollarBuy;
+    const saleDollarDiff = newDollarSale - oldDollarSale;
+    const buyEuroDiff = newEuroBuy - oldEuroBuy;
+    const saleEuroDiff = newEuroSale - oldEuroSale;
 
     const dollarEmodji = '💵';
     const euroEmodji = '💶';
-    const valutaMainText = `${dollarEmodji} buy: ${Number(dollarBuy).toFixed(2)}  sale: ${Number(dollarSale).toFixed(2)} 
-    \n${euroEmodji} buy: ${Number(euroBuy).toFixed(2)}  sale: ${Number(euroSale).toFixed(2)}`;
+    const valutaMainText = `${dollarEmodji} buy: ${Number(newDollarBuy).toFixed(2)}  sale: ${Number(newDollarSale).toFixed(2)}\n${dollarEmodji}Різниця на купівлю: ${buyDollarDiff.toFixed(2)} та продаж: ${saleDollarDiff.toFixed(2)}\n${euroEmodji} buy: ${Number(newEuroBuy).toFixed(2)}  sale: ${Number(newEuroSale).toFixed(2)}\n${euroEmodji}Різниця на купівлю: ${buyEuroDiff.toFixed(2)} та продаж: ${saleEuroDiff.toFixed(2)}`;
 
-    await bot.sendPhoto(chatId, __dirname + '/stonks-template.png', {caption: valutaMainText});
+    let sticker;
+    Math.sign(buyDollarDiff) === 1 || !Math.sign(buyDollarDiff) ? sticker = stonks_up : sticker = stonks_down;
+
+    await bot.sendSticker(chatId, sticker);
+    await bot.sendMessage(chatId, valutaMainText);
 }
 
 cron.schedule('0 9 * * *',  async () => await sendCurrentValues(rumunyChatId), {});
@@ -34,18 +56,26 @@ cron.schedule('0 15 * * *', async () => await sendCurrentValues(rumunyChatId), {
 cron.schedule('0 18 * * *', async () => await sendCurrentValues(rumunyChatId), {});
 cron.schedule('0 21 * * *', async () => await sendCurrentValues(rumunyChatId), {});
 
-let toBeOrNot2Be;
-
 bot.on('message',async msg => {
-    if (!msg.text && !msg.voice && !msg.video_note) return;
+    if (!msg.text && !msg.voice && !msg.video_note && !msg.sticker) return;
 
-    if (msg.voice) return await bot.sendMessage(msg.chat.id, 'Завали єбло.', {reply_to_message_id: msg.message_id});
-    if (msg.video_note) return await bot.sendMessage(msg.chat.id, 'Їбать ти уродіна, скройся.', {reply_to_message_id: msg.message_id});
+    if (msg.voice) {
+        let randomOnVoiceAnswer = random.integer(0, on_voice_answers.length - 1);
+        return await bot.sendMessage(msg.chat.id, on_voice_answers[randomOnVoiceAnswer], {reply_to_message_id: msg.message_id});
+    }
+    if (msg.video_note) {
+        let randomOnVideoAnswer = random.integer(0, on_video_answers.length - 1);
+        return await bot.sendMessage(msg.chat.id, on_video_answers[randomOnVideoAnswer], {reply_to_message_id: msg.message_id});
+    }
+    if ((msg.chat.type === 'private' && msg.sticker) || (msg.chat.type === 'supergroup' && msg.sticker && msg.reply_to_message && msg.reply_to_message.from.username === 'infoForUBot')) {
+        let randomOnStickerAnswer = random.integer(0, on_sticker_answers.length - 1);
+        return await bot.sendMessage(msg.chat.id, on_sticker_answers[randomOnStickerAnswer], {reply_to_message_id: msg.message_id});
+    }
 
     const inputMsg = msg.text.toLowerCase();
     switch (inputMsg) {
         case startCommand: {
-            await bot.sendSticker(msg.chat.id, 'CAACAgIAAxkBAAMSXxb41FtpP-0jAniGIsF1DLt0lZAAAmIAA_IEIBauBak-amt-jRoE');
+            await bot.sendSticker(msg.chat.id, defaultStickers[1].id);
             break;
         }
         case getValutaCommandPrivate:
@@ -59,18 +89,15 @@ bot.on('message',async msg => {
             break;
         }
         case getogoSticker: {
-            await bot.sendSticker(msg.chat.id, 'CAACAgIAAxkBAAIBml8YPxYIZqbaMdzvad_ZOw3AmRx6AAIcAQACtIBKJGEipCBAGzrcGgQ');
+            await bot.sendSticker(msg.chat.id, vinnik_ogo);
             break;
         }
         default: {
             if (msg.chat.type === 'supergroup') if (!msg.reply_to_message || (msg.reply_to_message && msg.reply_to_message.from.username !== 'infoForUBot')) return;
-            toBeOrNot2Be = Math.round((Math.random()));
 
-            let sticker;
+            let randomDefaultStickerIndex = random.integer(0, defaultStickers.length - 1);
 
-            toBeOrNot2Be ? sticker = 'CAACAgIAAxkBAAMSXxb41FtpP-0jAniGIsF1DLt0lZAAAmIAA_IEIBauBak-amt-jRoE' : sticker = 'CAACAgIAAxkBAAOEXxdbvKsXtIxwW01l3JlvfhzjArMAAsQFAAIjBQ0AAe5L2KGTqlu8GgQ';
-
-            await bot.sendSticker(msg.chat.id, sticker, {reply_to_message_id: msg.message_id});
+            await bot.sendSticker(msg.chat.id, defaultStickers[randomDefaultStickerIndex].id, {reply_to_message_id: msg.message_id});
         }
     }
 });
---
+++ constants/stickers.js
@@ -0,0 +1,99 @@
+module.exports = {
+    defaultStickers: [
+        {
+            name: 'ti_hochesh_menya_dovesti',
+            id: 'CAACAgIAAxkBAAOEXxdbvKsXtIxwW01l3JlvfhzjArMAAsQFAAIjBQ0AAe5L2KGTqlu8GgQ'
+        },
+        {
+            name: 'otebis_ot_menya',
+            id: 'CAACAgIAAxkBAAMSXxb41FtpP-0jAniGIsF1DLt0lZAAAmIAA_IEIBauBak-amt-jRoE'
+        },
+        {
+            name: 'mozhno_nenado',
+            id: 'CAACAgIAAxkBAAICFF8YfyJC22SHgUlxFLiLdWxD-WgZAAInAANdCOUtrSoP3unLdToaBA'
+        },
+        {
+            name: 'ha_tfu_v_ebalo',
+            id: 'CAACAgIAAxkBAAICFV8Yf1NdwSpLTffaWAes7ktHw6CZAAIjAANdCOUt3yPLoZrf3m8aBA'
+        },
+        {
+            name: 'left_fight_cat',
+            id: 'CAACAgIAAxkBAAICFl8Yf9o_ERw4_l6Gs_H9MoFd-Yh8AAJdAAPyBCAWUX_-IZD_mW8aBA'
+        },
+        {
+            name: 'zbagoyno',
+            id: 'CAACAgUAAxkBAAICF18YgEFw26DuhkMipgQGP7NtmJHoAALTAwAC6QrIAyxEf1v8EvO9GgQ'
+        },
+        {
+            name: 'da_nu_naher',
+            id: 'CAACAgIAAxkBAAICGF8YgHs08yO9Us4HeW0BqWUv8i1JAAIqAwACCqD_A19n5_Xpbr8wGgQ'
+        },
+        {
+            name: 'huli_tebe_nado',
+            id: 'CAACAgIAAxkBAAICGV8YgKm7xYFEnup-7u9oudXe6u-PAAIJDQACF3oKAAHfsS6S_dnydhoE'
+        },
+        {
+            name: 'nahuy_eto_tuda',
+            id: 'CAACAgIAAxkBAAICGl8YgNtjCNoZiYKwrje7xQbgoMvgAAKSAAOFQTIQqn4IdLiLi_waBA'
+        },
+        {
+            name: 'ruku_uberi',
+            id: 'CAACAgIAAxkBAAICG18YgSbq_TcUTRRGdNecYOw-tfyeAAJyAgACz151ErAk8RBLTtSPGgQ'
+        },
+        {
+            name: 'ya_ne_ponimau_ya_riba',
+            id: 'CAACAgIAAxkBAAICHF8YgVIIZRkIEg7hNgbj2cM1Y5VuAAI0AAOSnx4R8h1MdmUxNdAaBA'
+        },
+        {
+            name: 'baba_z_cheloveka_pauka',
+            id: 'CAACAgIAAxkBAAICHV8YgaXdPGt_irQBlNOQ2fzlWp-7AAKPAAMrLT8Z5zJMt6x3ZHkaBA'
+        },
+        {
+            name: 'olyasha_fight',
+            id: 'CAACAgIAAxkBAAICHl8YgeNQqOWImITnXKHqdw3i6Nk3AAI-AAOTmH4XbMCGbgbZM_4aBA'
+        },
+        {
+            name: 'viydi_rozbiynuk',
+            id: 'CAACAgIAAxkBAAICH18Ygf2DEUoIi6ToQxXCOG6LyAQXAAIyAAPTT_kYOjlD1OUNlrUaBA'
+        },
+        {
+            name: 'sho_tobi_vzhe_ye',
+            id: 'CAACAgIAAxkBAAICIF8YgjSinyLpfnVwu8vQRmsdwJ9qAAJJAANqbBkcn6c2Wg9lKnMaBA'
+        },
+        {
+            name: 'a_nihuya',
+            id: 'CAACAgIAAxkBAAICIV8YgmimzHQRnFDuOwc90hnhd0chAAJEAARx2BYAAdwPTqHDluMaBA'
+        },
+        {
+            name: 'ugomonis_chort',
+            id: 'CAACAgIAAxkBAAICIl8YgpUQFDjtFHeDQg-f4jIjIidRAAJcAARx2BZGfQiCuHQkNBoE'
+        },
+        {
+            name: 'zhuy_sraku',
+            id: 'CAACAgIAAxkBAAICI18YguRuKulqDrnafrgTXXuaWk5nAAI3AAMMap8S2KXXVq7DX7QaBA'
+        },
+        {
+            name: 'tu_cho_suka_tu_cho',
+            id: 'CAACAgIAAxkBAAICJF8Ygx9aJ2w_Wikf-VPLIb4fEqqbAAICAgACcQtCBVK6WK6cDdbcGgQ'
+        },
+        {
+            name: 'yibat_yarik_tu_travish',
+            id: 'CAACAgIAAxkBAAICJV8Yg1Uv9Pie6A1D0Ta_VYzIIeC5AAKQAAMkX30Hqt64P_dox_saBA'
+        },
+        {
+            name: 'fuck_you',
+            id: 'CAACAgIAAxkBAAICJl8Yg4_RjCxfvND30DHFdnhue6_8AAIBAQACN4QwAAEfGy4SsEhpiRoE'
+        },
+        {
+            name: 'che_tu_menya_besish',
+            id: 'CAACAgIAAxkBAAICJ18Yg6QH0rEPG6qBVClA9MupZ923AAKTAQACN4QwAAGlQUQU3-NFMxoE'
+        },
+        {
+            name: 'lapsha_na_ushi',
+            id: 'CAACAgIAAxkBAAICJ18Yg6QH0rEPG6qBVClA9MupZ923AAKTAQACN4QwAAGlQUQU3-NFMxoE'
+        }
+    ],
+    vinnik_ogo: 'CAACAgIAAxkBAAIBml8YPxYIZqbaMdzvad_ZOw3AmRx6AAIcAQACtIBKJGEipCBAGzrcGgQ',
+    stonks_up: 'CAACAgIAAxkBAAIB8F8Yemrw8DX-ngVk2NOIojZgxpVUAAJEAANVZu4qtsrXd1Y36poaBA',
+    stonks_down: 'CAACAgIAAxkBAAIB8V8YeoPsj4OEGrSr2OHUEGcQBGDrAAJFAANVZu4qJETPcEg5coAaBA'
+};
